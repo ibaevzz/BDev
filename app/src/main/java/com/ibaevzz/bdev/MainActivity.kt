45da459d479ae79006e340591c496cfc833ed3cb
Ibaev Zubai <ibaevzubic2004@gmail.com>
2024-01-07 18:55:10 +0300

    Добавлена некоторая функциональность

--- app/src/main/java/com/ibaevzz/bdev/MainActivity.kt
+++ app/src/main/java/com/ibaevzz/bdev/MainActivity.kt
@@ -17,7 +17,10 @@ import kotlinx.coroutines.withContext
 import java.io.IOException
 import java.io.InputStream
 import java.io.OutputStream
+import java.lang.Math.abs
+import java.lang.Math.round
 import java.util.*
+import kotlin.math.roundToInt
 
 
 class MainActivity : AppCompatActivity() {
@@ -123,5 +126,136 @@ class MainActivity : AppCompatActivity() {
             }
         }
         return result to (recData?:ByteArray(0))
+    }
+
+    private suspend fun enterPassword(address: ByteArray, nDevId: Int): Boolean{
+        val pIndex = if(nDevId in Constants.CURSED_IDS) 1 else 0
+        var result = 3
+        var recData: ByteArray
+        for(i in 0 until Constants.PULSAR_PASSWORDS.size){
+            val pass = Constants.PULSAR_PASSWORDS[i]
+            val pPass = toBytes(pass, 4)
+            val r = tryAttempts(PulsarFunc.injectCRC(address +
+                    Constants.PASSWORD_REQUEST[pIndex] + pPass +
+                    pPass + toBytes(i+1, 2)))
+            result = r.first
+            recData = r.second
+            if(result==1){
+                if(PulsarFunc.handlePulsarErrorCode(recData)==0){
+                    return true
+                }
+            }
+        }
+        return false
+    }
+
+    private suspend fun writeNewValue(address: ByteArray, newValue: Double, newAddress: Double, devId: Int): Triple<Int, ByteArray, Double>{
+        val multiplier = if(devId==98) 1 else 1000
+        var value: Double = ((newValue*1000).roundToInt().toDouble()/1000.0) * multiplier
+        var dataValue = ByteArray(4)
+        if(devId == 98){
+            dataValue = floatToHex(value)
+        }else{
+            dataValue = toBytes(value.toInt(), 4)
+            value /= 1000
+        }
+        val res = tryAttempts(PulsarFunc.injectCRC(address + Constants.WRITE_VALUE_REQUEST + dataValue + toBytes(1, 2)))
+        return Triple(res.first, res.second, value)
+    }
+
+    private fun toBytes(ch: Int, size: Int): ByteArray{
+        var s = ch.toString(16)
+        if(s.length%2==1){
+            s = "0$s"
+        }
+        val bytes = if (s.length/2<=size){
+            ByteArray(size)
+        }else{
+            ByteArray(s.length/2)
+        }
+        var i = s.length-1
+        var ii = 0
+        while(i>0){
+            bytes[ii] = (s[i-1].toString() + s[i].toString()).toUByte(16).toByte()
+            i-=2
+            ii+=1
+        }
+        while(ii<bytes.size){
+            bytes[ii] = 0
+            ii+=1
+        }
+        return bytes
+    }
+
+
+    //TODO неточные числа
+    private fun floatToHex(_ch: Double): ByteArray{
+        val ch = kotlin.math.abs(_ch)
+        var m = 0
+        if(ch==0.0){
+            return byteArrayOf(0, 0, 0, 0)
+        }else if(ch.toInt() == 0){
+            var mm = -1
+            for(i in drToBin(ch)){
+                if(i == '1'){
+                    m = mm
+                    break
+                }
+                mm -= 1
+            }
+        }else{
+            m = cToBin(ch).length - 1
+        }
+        val z = if(_ch<0) 1 else 0
+        val step = (m + 127).toString(2).padStart(8, '0')
+        var mant = ""
+        mant = if(m==0){
+            drToBin(ch).padEnd(23, '0')
+        }else if(m>0){
+            (cToBin(ch).substring(1) + drToBin(ch)).padEnd(23, '0')
+        }else{
+            drToBin(ch).substring(abs(m)).padEnd(23, '0')
+        }
+        println(cToBin(ch))
+        println(drToBin(ch))
+        var res = ""
+        var i = 0
+        val s = z.toString() + step + mant
+        while(i < s.length){
+            res += s.substring(i..i+3).toInt(2).toString(16)
+            i += 4
+        }
+        i = res.length-1
+        var ii = 0
+        val resB = ByteArray(4)
+        while(i>0){
+            resB[ii] = (res[i-1].toString() + res[i].toString()).toUByte(16).toByte()
+            ii += 1
+            i -= 2
+        }
+        return resB
+    }
+
+    private fun cToBin(a: Double): String{
+        val ch: Int = a.toInt()
+        return ch.toString(2)
+    }
+
+    private fun drToBin(a: Double): String{
+        var ch = a % 1
+        var s = ""
+        for(i in 0..22){
+            ch *= 2
+            if (ch>=1){
+                s+="1"
+                ch-=1
+            }else{
+                s+="0"
+            }
+            if(ch==0.0){
+                break
+            }
+        }
+        return s
     }
 }
